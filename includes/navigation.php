fa9333352a98ca91d2485d359edc0507790b9833
Michael <michaelscooterpoulsen@hotmail.com>
2023-05-13 20:40:18 +0200

    Installeret en nav menu

---
+++ includes/navigation.php
@@ -0,0 +1,45 @@
+<!-- Navbar - Start -->
+<nav class="navbar navbar-expand-lg bg-white fixed-top shadow">
+    <div class="container-fluid px-lg-5">
+        <a class="navbar-brand text-farve5" href="#">Mette og Martin</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Forside</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#om_os">Om os</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#koncerter">Koncerter</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#video">Nyeste video</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#referencer">Referencer</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#kontakt">Kontakt</a>
+                </li>
+            </ul>
+            <a href="#koncerter" class="btn btn-primary text-white">Koncerter</a>
+        </div>
+    </div>
+</nav>
+
+
+
+
