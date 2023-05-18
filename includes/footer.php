f2c9bd58242d5835a40a3fe8091cbb862d25f53d
Michael <michaelscooterpoulsen@hotmail.com>
2023-05-18 12:47:47 +0200

    Indsat mobil header billeder på events

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,62 @@
-<footer class="bg-secondary border-top border-white border-1">
-    <div class="container d-flex justify-content-center align-items-center p-3">
-        <span class="text ms-2 text-white">© 2023 mpportfolio.dk</span>
+<footer class="container-fluid bg-secondary text-white justify-content-around">
+    <div class="row text-center align-items-center py-3 d-flex">
+        <div class="col-12 col-lg-3 py-1 p-lg-0">
+            <div class="d-flex align-items-center justify-content-center">
+                <i class="fa-solid fa-phone"></i>
+                <p class="mb-0 ps-2">(+45) 54 85 03 55</p>
+            </div>
+        </div>
+        <div class="col-12 col-lg-3 py-1 p-lg-0">
+            <div class="d-flex align-items-center justify-content-center">
+                <i class="fa-solid fa-location-dot"></i>
+                <p class="mb-0 ps-2">Ndr. Kajgade/Sandgade (Kajen), 4800 Nykøbing F.</p>
+            </div>
+        </div>
+        <div class="col-12 col-lg-3 py-1 p-lg-0">
+            <div class="d-flex align-items-center justify-content-center">
+                <i class="fa-solid fa-envelope"></i>
+                <p class="mb-0 ps-2">vinoteque-m@rius.dk</p>
+            </div>
+        </div>
+        <div class="col-12 col-lg-3 py-1 p-lg-0">
+            <div class="d-flex align-items-center justify-content-center">
+                <i class="fa-solid fa-door-open"></i>
+                <p class="mb-0 ps-2">Åben: Tors. kl. 16-23 & Fre.-Lør. kl. 16-24</p>
+            </div>
+        </div>
     </div>
 </footer>
+
+
+<footer class="container-fluid bg-secondary text-white">
+    <div class="boks1 d-flex justify-content-center g-0 mx-auto">
+        <div class="row text-center align-items-center py-3">
+            <div class="col-12 col-lg-3 py-1 p-lg-0 mx-auto">
+                <div class="d-flex align-items-center justify-content-center">
+                    <i class="fa-solid fa-phone"></i>
+                    <p class="mb-0 ps-2">(+45) 54 85 03 55</p>
+                </div>
+            </div>
+            <div class="col-12 col-lg-3 py-1 p-lg-0">
+                <div class="d-flex align-items-center justify-content-center">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <p class="mb-0 ps-2">Ndr. Kajgade/Sandgade (Kajen), 4800 Nykøbing F.</p>
+                </div>
+            </div>
+            <div class="col-12 col-lg-3 py-1 p-lg-0">
+                <div class="d-flex align-items-center justify-content-center">
+                    <i class="fa-solid fa-envelope"></i>
+                    <p class="mb-0 ps-2">vinoteque-m@rius.dk</p>
+                </div>
+            </div>
+            <div class="col-12 col-lg-3 py-1 p-lg-0">
+                <div class="d-flex align-items-center justify-content-center">
+                    <i class="fa-solid fa-door-open"></i>
+                    <p class="mb-0 ps-2">Åben: Tors. kl. 16-23 & Fre.-Lør. kl. 16-24</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
