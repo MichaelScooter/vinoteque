82550d78f391f9e42c0cee03c9b0cf8383777981
Michael <michaelscooterpoulsen@hotmail.com>
2023-05-23 12:18:30 +0200

    - Tilført Favicon

--- includes/head.php
+++ includes/head.php
@@ -1,3 +1,12 @@
 <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
 <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="180x180" href="http://mpportfolio.dk/vinoteque/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="http://mpportfolio.dk/vinoteque/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="http://mpportfolio.dk/vinoteque/icons/favicon-16x16.png">
+<link rel="manifest" href="http://mpportfolio.dk/vinoteque/icons/site.webmanifest">
+<link rel="mask-icon" href="http://mpportfolio.dk/vinoteque/icons/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="http://mpportfolio.dk/vinoteque/icons/favicon.ico">
+<link rel="icon" href="http://mpportfolio.dk/vinoteque/icons/favicon.ico">
