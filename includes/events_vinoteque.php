e98f488683463eed3646f246df6aa9f5aaa49c1e
Michael <michaelscooterpoulsen@hotmail.com>
2023-05-31 12:07:53 +0200

    - Uploadet alle Webp avatar billeder

--- includes/events_vinoteque.php
+++ includes/events_vinoteque.php
@@ -8,8 +8,12 @@
                     <h6 class="pt-lg-4 text-white">Lørdag kl. 20.00</h6>
                     <h4 class="text-white pt-lg-3">øl<br>Smagning</h4>
                     <h5 class="text-white py-lg-3">195,- pr. person</h5>
-                    <img src="images/oel-smagning-vinbaren.jpg" alt="Udforsk en verden af håndværksøl ved vores spændende ølsmagning på Vinoteque" class="img-fluid h-100 rounded-4 border border-white border-1">
-                    <a href="oel_smagning.php" class="btn btn-primary text-white btn-link mt-4">Se event</a>
+                    <picture>
+                        <source srcset="images/oel-smagning-vinbaren.webp" type="image/webp">
+                        <source srcset="images/oel-smagning-vinbaren.jpg" type="image/jpeg">
+                        <img src="images/oel-smagning-vinbaren.jpg" alt="Udforsk en verden af håndværksøl ved vores spændende ølsmagning på Vinoteque" class="img-fluid h-100 rounded-4 border border-white border-1">
+                    </picture>
+                      <a href="oel_smagning.php" class="btn btn-primary text-white btn-link mt-4">Se event</a>
                 </div>
             </div>
             <div class="col-6 col-lg-3">
@@ -21,8 +25,12 @@
                     <h6 class="pt-lg-4 text-white">Lørdag kl. 16.00</h6>
                     <h4 class="text-white pt-lg-3">Sønderjysk<br>Kagebord</h4>
                     <h5 class="text-white py-lg-3">245,- pr. person</h5>
-                    <img src="images/kagebuffet-vinbaren.jpg" alt="Forkæl dine smagsløg med sønderjyske delikatesser ved vores overdådige kagebord på Vinoteque" class="img-fluid h-100 rounded-4 border border-white border-1">
-                    <a href="kagebord.php" class="btn btn-primary text-white btn-link mt-4">Se event</a>
+                    <picture>
+                        <source srcset="images/kagebuffet-vinbaren.webp" type="image/webp">
+                        <source srcset="images/kagebuffet-vinbaren.jpg" type="image/jpeg">
+                        <img src="images/kagebuffet-vinbaren.jpg" alt="Forkæl dine smagsløg med sønderjyske delikatesser ved vores overdådige kagebord på Vinoteque" class="img-fluid h-100 rounded-4 border border-white border-1">
+                    </picture>
+                      <a href="kagebord.php" class="btn btn-primary text-white btn-link mt-4">Se event</a>
                 </div>
             </div>
             <div class="col-6 col-lg-3 pt-5 pt-lg-0">
@@ -34,8 +42,12 @@
                     <h6 class="pt-lg-4 text-white">Lørdag kl. 20.00</h6>
                     <h4 class="text-white pt-lg-3">Gin<br>Smagning</h4>
                     <h5 class="text-white py-lg-3">245,- pr. person</h5>
-                    <img src="images/gin-smagning-vinbaren.jpg" alt="Opdag de skønne smagsnuancer af gin ved vores eksklusive gin smagning på Vinoteque" class="img-fluid h-100 rounded-4 border border-white border-1">
-                    <a href="gin.php" class="btn btn-primary text-white btn-link mt-4">Se event</a>
+                    <picture>
+                        <source srcset="images/gin-smagning-vinbaren.webp" type="image/webp">
+                        <source srcset="images/gin-smagning-vinbaren.jpg" type="image/jpeg">
+                        <img src="images/gin-smagning-vinbaren.jpg" alt="Opdag de skønne smagsnuancer af gin ved vores eksklusive gin smagning på Vinoteque" class="img-fluid h-100 rounded-4 border border-white border-1">
+                    </picture>
+                     <a href="gin.php" class="btn btn-primary text-white btn-link mt-4">Se event</a>
                 </div>
             </div>
             <div class="col-6 col-lg-3 pt-5 pt-lg-0">
@@ -47,7 +59,11 @@
                     <h6 class="pt-lg-4 text-white">Lørdag kl. 20.00</h6>
                     <h4 class="text-white pt-lg-3">Jazz<br>Koncert</h4>
                     <h5 class="text-white py-lg-3">295,- pr. person</h5>
-                    <img src="images/jazz-koncert-vinbaren.jpg" alt="Nyd den bløde rytme og stemningen til vores jazzaften på Vinoteque" class="img-fluid h-100 rounded-4 border border-white border-1">
-                    <a href="jazz.php" class="btn btn-primary text-white btn-link mt-4">Se event</a>
+                    <picture>
+                        <source srcset="images/jazz-koncert-vinbaren.webp" type="image/webp">
+                        <source srcset="images/jazz-koncert-vinbaren.jpg" type="image/jpeg">
+                        <img src="images/jazz-koncert-vinbaren.jpg" alt="Nyd den bløde rytme og stemningen til vores jazzaften på Vinoteque" class="img-fluid h-100 rounded-4 border border-white border-1">
+                    </picture>
+                     <a href="jazz.php" class="btn btn-primary text-white btn-link mt-4">Se event</a>
                 </div>
             </div>
